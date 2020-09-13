4859f3b0482601c07da881189e0db2641523035a
javier alejandro castañeda celis <jcastaeda10@gmail.com>
2020-09-13 15:17:52 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { PhotoFormComponent } from './components/photo-form/photo-form.component';
+import { PhotoPreviewComponent } from './components/photo-preview/photo-preview.component';
+import { PhotoListComponent } from './components/photo-list/photo-list.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    PhotoFormComponent,
+    PhotoPreviewComponent,
+    PhotoListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
